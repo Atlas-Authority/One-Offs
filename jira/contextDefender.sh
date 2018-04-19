400f3678b0f7811c0f9177e891a17a3b18fa3d94
Boris Berenberg <boris@atlasauthority.com>
2018-04-19 19:27:48 -0400

    JRASERVER-27957

---
+++ jira/contextDefender.sh
@@ -0,0 +1,9 @@
+// Addresses https://jira.atlassian.com/browse/JRASERVER-27957
+
+var contextDefender = function(){
+	return confirm("Are you sure you want to delete this scheme?");
+}
+
+$('[title="Delete Scheme"]').each(function(index,element){
+	element.onclick=contextDefender;
+})
