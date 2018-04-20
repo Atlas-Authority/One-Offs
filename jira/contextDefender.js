9cf81dd2b535edd1290f5a43d8425fdfd1d0a566
Boris Berenberg <boris@atlasauthority.com>
2018-04-20 19:06:56 -0400

    fixing file name and message

--- jira/contextDefender.js
+++ jira/contextDefender.js
@@ -1,7 +1,7 @@
 // Addresses https://jira.atlassian.com/browse/JRASERVER-27957
 
 var contextDefender = function(){
-	return confirm("Are you sure you want to delete this scheme?");
+	return confirm("Are you sure you want to delete this context?");
 }
 
 $('[title="Delete Scheme"]').each(function(index,element){
