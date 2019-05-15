2bed4a0f968b68cb2b2ab54567e5ba23bfbef180
Boris Berenberg <boris@atlasauthority.com>
2019-05-15 17:16:06 -0400

    new dvcs adding script

---
+++ jira/addDVCSRepos.sh
@@ -0,0 +1,14 @@
+This works for Bitbucket and GitHub, but has not been tested with GitHub Enterprise.
+
+// Get your repo list 
+curl --request GET \
+  --url http://<BASE URL>/rest/bitbucket/1.0/repositories \
+  --header 'accept: application/json' \
+  --header 'authorization: Basic <AUTH INFO>' 
+
+// Add the repo to Jira to sync
+curl --request POST \
+  --url http://<BASEURL>/rest/bitbucket/1.0/repo/<REPO ID FROM PREVIOUS STEP>/autolink \
+  --header 'authorization: Basic <AUTH INFO>' \
+  --header 'content-type: application/json' \
+  --data '{"payload": true}'
