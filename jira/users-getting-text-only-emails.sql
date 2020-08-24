59ebca2f46cfaf661c58e35e289b51fa09bc9845
Boris Berenberg <boris@atlasauthority.com>
2020-08-24 13:20:10 -0400

    adds active user filtering to text only email sql

--- jira/users-getting-text-only-emails.sql
+++ jira/users-getting-text-only-emails.sql
@@ -1,11 +1,14 @@
 select
   au.lower_user_name
   ,ps.propertyvalue
-from app_user au
+from cwd_user u, app_user au
 join propertyentry pe
   on au.id = pe.entity_id
   and pe.entity_name = 'ApplicationUser'
   and pe.property_key = 'user.notifications.mimetype'
 join propertystring ps
   on pe.id = ps.id
-where ps.propertyvalue = 'text';
+where
+u.lower_user_name = au.lower_user_name
+AND u.active = 1
+AND ps.propertyvalue = 'text';
