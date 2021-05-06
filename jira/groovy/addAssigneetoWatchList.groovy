0b5074843e434cc918a812792e068da30f906b1d
mikeAtlasAuthority <83780937+mikeAtlasAuthority@users.noreply.github.com>
2021-05-06 14:44:11 -0400

    Update addAssigneetoWatchList.groovy
    Cleaned up script

--- jira/groovy/addAssigneetoWatchList.groovy
+++ jira/groovy/addAssigneetoWatchList.groovy
@@ -1,29 +1,20 @@
 /*
 Title: Add Assignee to Watcher List
-Author - Mike Schultz - mschultz@zenefits.com
+Author - Mike Schultz - mike@atlasauthority.com
 Date Created: July 11, 2019
 Date Last Updated: N/A
 Description: Add Issue Assignee to the Issue's Watchers list upon issue assignment.
 */
 
-
+// Trigger event is IssueAssigned
 import com.atlassian.jira.component.ComponentAccessor
-import com.atlassian.jira.event.issue.AbstractIssueEventListener
-import com.atlassian.jira.event.issue.IssueEvent
-import com.atlassian.jira.issue.Issue
-import org.apache.log4j.Logger
-import com.atlassian.jira.ComponentManager
-import static org.apache.log4j.Level.DEBUG
 import com.atlassian.jira.issue.Issue
 import com.atlassian.jira.user.ApplicationUser
 import com.atlassian.jira.issue.watchers.WatcherManager
 
+Issue issue = event.getIssue()
+ApplicationUser assignee = issue.assignee
 def watcherManager = ComponentAccessor.getWatcherManager()
 def userManager = ComponentAccessor.getUserManager()
-Issue issue = event.getIssue()
-log.setLevel(DEBUG)
-log.debug "Event: ${event.getEventTypeId()} fired for ${issue} and caught by TaskVersionListener"
-def assignee = issue.assigneeId
-ApplicationUser assigneeWatcher = issue.assignee
-log.debug "assigneeWatcher: " + assigneeWatcher
-watcherManager.startWatching(assigneeWatcher, issue)
+
+watcherManager.startWatching(assignee, issue)
