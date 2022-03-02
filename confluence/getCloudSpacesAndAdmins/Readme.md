ce7dd61e658ab67bd60a40b415a0163942ca47df
Boris Berenberg <boris@atlasauthority.com>
2022-03-02 17:51:18 -0500

    adding a way to pull cloud admins

---
+++ confluence/getCloudSpacesAndAdmins/Readme.md
@@ -0,0 +1,10 @@
+# Overview
+
+Prints a CSV to console of all of the users and permissions in Confluence
+
+# Usage
+
+* Update the script with your username and API key and URL
+* run npm install
+* this was built with Node v14.19.0 and npm 6.14.16  
+* Run the script
---
+++ confluence/getCloudSpacesAndAdmins/getCloudSpacesAndAdmins.js
@@ -0,0 +1,48 @@
+const fetch = require('node-fetch');
+
+
+let url = 'https://example.atlassian.net/wiki/rest/api/space?expand=permissions';
+let username = 'username';
+let password = 'make sure to use an API key not a password';
+
+
+fetch(url, {
+  method: 'GET',
+   headers: {
+    'Authorization': 'Basic ' + Buffer.from(username + ":" + password).toString('base64'),
+    'Accept': 'application/json'
+   }
+})
+  .then(response => {
+    //console.log(
+    //  `Response: ${response.status} ${response.statusText}`
+    //);
+
+    return response.json();
+  })
+  .then(response => {
+    const results = response.results;
+
+    console.log("space_key,space_name,admin_display_name,user_type")
+
+    for (r in results){
+
+      let result = results[r]
+
+      let permissions = result.permissions
+
+      for (p in permissions){
+
+        let permission = permissions[p]
+
+        if (permission.operation.operation == 'administer' && permission.operation.targetType == 'space' && permission.subjects.user){
+
+            console.log(result.key + "," + result.name + "," + permission.subjects.user.results[0].displayName + ',' + permission.subjects.user.results[0].accountType)
+          
+        }
+      }
+
+    }
+    
+  })
+  .catch(err => console.error(err));
